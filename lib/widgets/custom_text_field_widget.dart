0650d23163af600c86b15db4bb4475a17d3e095a
beshprakash11 <beshprakashktm@gmail.com>
2024-03-28 13:48:50 +0100

    TextField added

--- lib/widgets/custom_text_field_widget.dart
+++ lib/widgets/custom_text_field_widget.dart
@@ -17,6 +17,28 @@ class CustomTextFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TextField(
+      controller: editingController,
+      decoration: InputDecoration(
+        labelText: labelText,
+        prefixIcon: iconData != null
+            ? Icon(iconData)
+            : Padding(
+                padding: EdgeInsets.all(8),
+                child: Image.asset(
+                  assetRef.toString(),
+                ),
+              ),
+        labelStyle: const TextStyle(
+          fontSize: 18,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+    );
   }
 }
