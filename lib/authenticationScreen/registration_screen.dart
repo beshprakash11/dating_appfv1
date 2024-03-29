033a60b5cc49603ac698929ac3394ee6af41dd77
beshprakash11 <beshprakashktm@gmail.com>
2024-03-29 14:12:26 +0100

    person field added

--- lib/authenticationScreen/registration_screen.dart
+++ lib/authenticationScreen/registration_screen.dart
@@ -8,8 +8,16 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  //Personal info
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+  TextEditingController nameTextEditingController = TextEditingController();
+  TextEditingController ageTextEditingController = TextEditingController();
+  TextEditingController phoneNoTextEditingController = TextEditingController();
+  TextEditingController cityTextEditingController = TextEditingController();
+  TextEditingController countryTextEditingController = TextEditingController();
+  TextEditingController profileHeadingTextEditingController =
+      TextEditingController();
 
   bool showProgressBar = false;
   @override
