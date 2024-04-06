b02945fbcc647f16c641a95b06352216a8537c87
beshprakash11 <beshprakashktm@gmail.com>
2024-04-06 11:50:34 +0200

    pickImageFileFormGallery created

--- lib/controllers/authentication_controller.dart
+++ lib/controllers/authentication_controller.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AuthenticationController extends GetxController {
   static AuthenticationController authController = Get.find();
+  late Rx<File?> pickedFile;
+  File? get profileImage => pickedFile.value;
+
+  pickImageFileFormGallery() async {
+    await ImagePicker().pickImage(source: ImageSource.gallery);
+  }
 }
