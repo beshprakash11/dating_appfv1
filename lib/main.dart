bdf1af5f63b7300a22f0614300fe53f21adb1332
beshprakash11 <beshprakashktm@gmail.com>
2024-04-08 10:15:35 +0200

    project updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) {
+  await Firebase
+          .initializeApp() //options: DefaultFirebaseOptions.currentPlatform)
+      .then((value) {
     Get.put(AuthenticationController());
   });
 
