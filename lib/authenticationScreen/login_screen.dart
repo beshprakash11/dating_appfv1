e91137281f0ddd4e0f24a8302fd7e3398a53165e
beshprakash11 <beshprakashktm@gmail.com>
2024-04-11 13:40:55 +0200

    login screen added

---
+++ lib/authenticationScreen/login_screen.dart
@@ -0,0 +1,159 @@
+import 'package:dating_appfv1/authenticationScreen/registration_screen.dart';
+import 'package:dating_appfv1/widgets/custom_text_field_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
+  bool showProgressBar = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 120,
+            ),
+            Image.asset(
+              "images/logo.png",
+              width: 300,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Login now to find your best Match!",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+
+            //email field
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 36,
+              height: 55,
+              child: CustomTextFieldWidget(
+                editingController: emailTextEditingController,
+                labelText: "Email",
+                iconData: Icons.email_outlined,
+                isObscure: false,
+              ),
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            //password field
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 36,
+              height: 55,
+              child: CustomTextFieldWidget(
+                editingController: passwordTextEditingController,
+                labelText: "Password",
+                iconData: Icons.lock_outline,
+                isObscure: true,
+              ),
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            //login button
+            Container(
+              width: MediaQuery.of(context).size.width - 36,
+              height: 55,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(12),
+                ),
+              ),
+              child: InkWell(
+                onTap: () {},
+                child: const Center(
+                  child: Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            //create account
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don't have an account?",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                InkWell(
+                  onTap: () {
+                    Get.to(() => const RegistrationScreen());
+                  },
+                  child: const Text(
+                    "Create here",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(
+              height: 24,
+            ),
+
+            showProgressBar == true
+                ? const CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.pink),
+                  )
+                : Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
