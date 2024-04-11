c5b781adbff98a5c82b5fbe6f38c90366b2b5ee0
beshprakash11 <beshprakashktm@gmail.com>
2024-04-11 13:31:55 +0200

    fire base init updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:dating_appfv1/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyABOs5zw1kF12_LXAQjIsd0nPYDwT0T89M",
+          authDomain: "dating-app-eb8d5.firebaseapp.com",
+          projectId: "dating-app-eb8d5",
+          storageBucket: "dating-app-eb8d5.appspot.com",
+          messagingSenderId: "499981639119",
+          appId: "1:499981639119:web:109497be6852a5666146ba"),
+    );
+  }
   runApp(const MyApp());
 }
 
