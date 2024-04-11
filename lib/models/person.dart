7d36cca68eb61ece3585f57251d659f46c4f01bf
beshprakash11 <beshprakashktm@gmail.com>
2024-04-11 14:39:36 +0200

    Person info added

--- lib/models/person.dart
+++ lib/models/person.dart
@@ -1,4 +1,10 @@
 class Person {
   //Personal info
   String? name;
+  String? age;
+  String? phoneNo;
+  String? city;
+  String? country;
+  String? profileHeading;
+  String? lookingForInaPartner;
 }
