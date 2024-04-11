7151f0c9dc855db4c2296c321551dbb63c1578b6
beshprakash11 <beshprakashktm@gmail.com>
2024-04-11 12:43:33 +0200

    login screen updated in main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dating_appfv1/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Text('Flutter Demo Home Page'),
+      home: const Login(),
     );
   }
 }
