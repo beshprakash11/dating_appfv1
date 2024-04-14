ddc6de9f5178622278cf6e7092dbac603753a886
beshprakash11 <beshprakashktm@gmail.com>
2024-04-14 08:31:52 +0200

    LikeSentLikeRecivedScreen page created

---
+++ lib/tabScreens/like_sent_like_recived_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LikeSentLikeRecivedScreen extends StatefulWidget {
+  const LikeSentLikeRecivedScreen({Key? key}) : super(key: key);
+
+  @override
+  _LikeSentLikeRecivedScreenState createState() =>
+      _LikeSentLikeRecivedScreenState();
+}
+
+class _LikeSentLikeRecivedScreenState extends State<LikeSentLikeRecivedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
