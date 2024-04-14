6e8c78eb06387167307eef8a36e3cf3e25d18509
beshprakash11 <beshprakashktm@gmail.com>
2024-04-14 08:15:55 +0200

    FavoriteSentFavoriteRecievedScreen page created

---
+++ lib/tabScreens/favorite_sent_favorite_recieved_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FavoriteSentFavoriteRecievedScreen extends StatefulWidget {
+  const FavoriteSentFavoriteRecievedScreen({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteSentFavoriteRecievedScreenState createState() =>
+      _FavoriteSentFavoriteRecievedScreenState();
+}
+
+class _FavoriteSentFavoriteRecievedScreenState
+    extends State<FavoriteSentFavoriteRecievedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
