e20e2577a69059780ef4037c952db63e6bb796fb
beshprakash11 <beshprakashktm@gmail.com>
2024-04-14 08:48:50 +0200

    tabScreensList and screen index updated

--- lib/homeScreen/home_screen.dart
+++ lib/homeScreen/home_screen.dart
@@ -1,3 +1,8 @@
+import 'package:dating_appfv1/tabScreens/favorite_sent_favorite_recieved_screen.dart';
+import 'package:dating_appfv1/tabScreens/like_sent_like_recived_screen.dart';
+import 'package:dating_appfv1/tabScreens/swipping_screen.dart';
+import 'package:dating_appfv1/tabScreens/user_details_screen.dart';
+import 'package:dating_appfv1/tabScreens/view_sent_view_received_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +13,14 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int screenIndex = 0;
+  List tabScreensList = [
+    const SwippingScreen(),
+    const ViewSentViewReceivedScreen(),
+    const FavoriteSentFavoriteRecievedScreen(),
+    const LikeSentLikeRecivedScreen(),
+    const UserDetailsScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
