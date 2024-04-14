8f0da16af6eff4163e72c9a71c537867b2ead7f6
beshprakash11 <beshprakashktm@gmail.com>
2024-04-14 08:41:45 +0200

    UserDetailsScreen created

---
+++ lib/tabScreens/user_details_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserDetailsScreen extends StatefulWidget {
+  const UserDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  _UserDetailsScreenState createState() => _UserDetailsScreenState();
+}
+
+class _UserDetailsScreenState extends State<UserDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
