62a651e0ce129473951173742e07d3ba00c7750b
beshprakash11 <beshprakashktm@gmail.com>
2024-04-14 08:13:26 +0200

    ViewSentViewReceivedScreen tab screen created

---
+++ lib/tabScreens/view_sent_view_received_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ViewSentViewReceivedScreen extends StatefulWidget {
+  const ViewSentViewReceivedScreen({Key? key}) : super(key: key);
+
+  @override
+  _ViewSentViewReceivedScreenState createState() =>
+      _ViewSentViewReceivedScreenState();
+}
+
+class _ViewSentViewReceivedScreenState
+    extends State<ViewSentViewReceivedScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
