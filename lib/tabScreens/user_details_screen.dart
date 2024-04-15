be4cb27bbce162cdd9a203ec53b3459e54182e4f
beshprakash11 <beshprakashktm@gmail.com>
2024-04-15 16:21:06 +0200

    retrieveUserInfo method created

--- lib/tabScreens/user_details_screen.dart
+++ lib/tabScreens/user_details_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,10 @@ class UserDetailsScreen extends StatefulWidget {
 }
 
 class _UserDetailsScreenState extends State<UserDetailsScreen> {
+  retrieveUserInfo() {
+    FirebaseFirestore.instance.collection("users").doc();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
