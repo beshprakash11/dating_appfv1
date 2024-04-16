5570e8a20b07f319169276ded0d6c2fe786677b5
beshprakash11 <beshprakashktm@gmail.com>
2024-04-16 08:48:14 +0200

    document var updated

--- lib/controllers/profile-controller.dart
+++ lib/controllers/profile-controller.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dating_appfv1/global.dart';
 import 'package:dating_appfv1/models/person.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
 class ProfileController extends GetxController {
@@ -30,5 +32,12 @@ class ProfileController extends GetxController {
   }
 
   //FavoriteSentAndFavoriteReceived
-  favoriteSentAndFavoriteReceived(String toUserID, String senderName) async {}
+  favoriteSentAndFavoriteReceived(String toUserID, String senderName) async {
+    var document = await FirebaseFirestore.instance
+        .collection("users")
+        .doc(toUserID)
+        .collection("favoriteReceived")
+        .doc(currentUserID)
+        .get();
+  }
 }
