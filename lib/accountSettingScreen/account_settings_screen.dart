dd9490c4afebbe9a11491aa750d4d404df072734
beshprakash11 <beshprakashktm@gmail.com>
2024-04-18 09:57:25 +0200

    account setting screen updated

--- lib/accountSettingScreen/account_settings_screen.dart
+++ lib/accountSettingScreen/account_settings_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class AccountSettingsScreen extends StatefulWidget {
@@ -8,10 +10,25 @@ class AccountSettingsScreen extends StatefulWidget {
 }
 
 class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
+  bool uploading = false, next = false;
+  final List<File> _image = [];
+  List<String> urlList = [];
+  double val = 0;
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Account Setting Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          next ? "Profile Information" : "Choose 5 Images",
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text("Account Setting Screen"),
+      ),
     );
   }
 }
