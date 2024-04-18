26d2be32a6194d2e29272dc3586268d13885f044
beshprakash11 <beshprakashktm@gmail.com>
2024-04-18 10:22:27 +0200

    _buildImageDisplay method created

--- lib/accountSettingScreen/account_settings_screen.dart
+++ lib/accountSettingScreen/account_settings_screen.dart
@@ -14,6 +14,8 @@ class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
   final List<File> _image = [];
   List<String> urlList = [];
   double val = 0;
+
+  chooseImage() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,13 +49,42 @@ class _AccountSettingsScreenState extends State<AccountSettingsScreen> {
         Container(
           padding: const EdgeInsets.all(4),
           child: GridView.builder(
+            itemCount: _image.length + 1,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
             ),
-            itemBuilder: (context, index) {},
+            itemBuilder: (context, index) {
+              return index == 0
+                  ? Container(
+                      color: Colors.white30,
+                      child: Center(
+                        child: IconButton(
+                          onPressed: () {
+                            !uploading ? chooseImage() : null;
+                          },
+                          icon: const Icon(Icons.add),
+                        ),
+                      ),
+                    )
+                  : _buildImageDisplay(index);
+            },
           ),
         ),
       ],
+    );
+  }
+
+  Container _buildImageDisplay(int index) {
+    return Container(
+      margin: const EdgeInsets.all(3),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: FileImage(
+            _image[index - 1],
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
