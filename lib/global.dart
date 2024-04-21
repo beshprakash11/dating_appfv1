7bfcbecf220aec4396073392652bad323edac6a6
beshprakash11 <beshprakashktm@gmail.com>
2024-04-21 08:44:52 +0200

    fcmServerToken added in gbobal created

--- lib/global.dart
+++ lib/global.dart
@@ -1,3 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 String currentUserID = FirebaseAuth.instance.currentUser!.uid;
+String fcmServerToken =
+    "key=AAAAdGk6Xc8:APA91bGahsn71HHaJQh-QgD_SVVLoVaa9gcQkCXaDWTT_9UcLBb96-yX_2u3tzzmXIIUeJuCzeayMihWLtuVAR5zve-4UIvDRYvUB2fvc5EyvgMdJaaN5M6XYGqtU7XqrcL-IHg2DHQS";
