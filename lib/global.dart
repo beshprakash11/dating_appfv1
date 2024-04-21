8e248c94f1330a45bc6b838ff59032d31427b4e6
beshprakash11 <beshprakashktm@gmail.com>
2024-04-21 10:01:34 +0200

    global variable updated

--- lib/global.dart
+++ lib/global.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 String currentUserID = FirebaseAuth.instance.currentUser!.uid;
+String? chooseAge;
+String? choosenCountry;
+String? choosenGender;
 String fcmServerToken =
     "key=AAAAdGk6Xc8:APA91bGahsn71HHaJQh-QgD_SVVLoVaa9gcQkCXaDWTT_9UcLBb96-yX_2u3tzzmXIIUeJuCzeayMihWLtuVAR5zve-4UIvDRYvUB2fvc5EyvgMdJaaN5M6XYGqtU7XqrcL-IHg2DHQS";
