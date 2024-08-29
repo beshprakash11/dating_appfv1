bf942dd03bfbfb3b917900636f5dd176aca01839
beshprakash11 <beshprakashktm@gmail.com>
2024-08-29 08:32:54 +0200

    view_sent_view_received_screen updated

--- lib/tabScreens/view_sent_view_received_screen.dart
+++ lib/tabScreens/view_sent_view_received_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:xbsdating/global.dart';
 
 class ViewSentViewReceivedScreen extends StatefulWidget {
   const ViewSentViewReceivedScreen({super.key});
