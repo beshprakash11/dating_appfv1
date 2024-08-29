1bac178c8b7197f1bf99ee0270eeefef697a075c
beshprakash11 <beshprakashktm@gmail.com>
2024-08-29 08:31:21 +0200

    swipping_screen updated

--- lib/tabScreens/swipping_screen.dart
+++ lib/tabScreens/swipping_screen.dart
@@ -4,6 +4,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:xbsdating/controllers/profile-controller.dart';
+import 'package:xbsdating/global.dart';
+import 'package:xbsdating/tabScreens/user_details_screen.dart';
 
 class SwippingScreen extends StatefulWidget {
   const SwippingScreen({super.key});
