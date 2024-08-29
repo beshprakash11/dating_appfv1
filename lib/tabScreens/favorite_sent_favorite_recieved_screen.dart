60e27b5bda33123d175a785bb0db702db2359546
beshprakash11 <beshprakashktm@gmail.com>
2024-08-29 08:29:54 +0200

    favorte sent received updated

--- lib/tabScreens/favorite_sent_favorite_recieved_screen.dart
+++ lib/tabScreens/favorite_sent_favorite_recieved_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:xbsdating/global.dart';
 
 class FavoriteSentFavoriteRecievedScreen extends StatefulWidget {
   const FavoriteSentFavoriteRecievedScreen({super.key});
